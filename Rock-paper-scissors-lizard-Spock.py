7958e6b3f9463a4b06c40f3f6319c1cd92c6fa32
AdemRamadani <ademramadani@outlook.com>
2015-06-17 00:40:40 +0200

    Second commit

--- Rock-paper-scissors-lizard-Spock.py
+++ Rock-paper-scissors-lizard-Spock.py
@@ -1,65 +1,81 @@
-# Rock-paper-scissors-lizard-Spock
-
-# The key idea of this program is to equate the strings
-# "rock", "paper", "scissors", "lizard", "Spock" to numbers
-# as follows:
-#
+# Rock-Paper-Scissors-Lizard-Spock
 # 0 - rock
 # 1 - Spock
 # 2 - paper
 # 3 - lizard
 # 4 - scissors
+
 import random
 
 # helper functions
 
+#assign name to given number
 def number_to_name(number):
-    if number == 0:
-        return "rock"
-    elif number == 1:
-        return "Spock"
-    elif number == 2:
-        return "paper"
-    elif number == 3:
-        return "lizard"
-    elif number == 4:
-        return "scissors"
-    elif number<0 or number >5:
-        return "Incorrect value"
-        
-    
-def name_to_number(name):
-     if name=="rock":
-        return 0
-     elif name=="Spock":
-        return 1
-     elif name=="paper":
-        return 2
-     elif name == "lizard":
-        return 3
-     elif name == "scissors":
-        return 4
-     elif name!="rock" or name!="Spock" or name!="paper" or name != "lizard" or name!= "scissors":
-        return "Incorrect value"
-
-
-def rpsls(name): 
-    player_number = name_to_number(name);
-    comp_number = random.randrange(0,5)
-    mod = (comp_number-player_number) % 5
-    
-    print
-    print "Player chooses " + name
-    print "Computer chooses " + number_to_name(comp_number)
-    if mod == 0:
-        print "Draw!"
-    elif mod == 1 or mod == 2:
-        print "Computer wins!"
+    if (number == 0):
+        name = "rock"
+    elif (number == 1):
+        name = "spock"
+    elif (number == 2):
+        name = "paper"
+    elif (number == 3):
+        name = "lizard"
+    elif (number == 4):
+        name = "scissors"
     else:
-        print "Player wins!"
+        name = "null"
+    return name                
 
-   
-# test code
+#assign number to given name
+def name_to_number(name):
+    if (name == "rock"):
+        num = 0
+    elif (name == "spock"):
+        num = 1
+    elif (name == "paper"):
+        num = 2
+    elif (name == "lizard"):
+        num = 3
+    elif (name == "scissors"):
+        num = 4
+    else:
+        num = 5
+    return num
+
+#decide winner
+def rpsls(guess):
+    
+    player_number = name_to_number(guess)
+    
+    #Print player's choice
+    
+    print "Player chooses",guess
+    
+    #Select random computer's choice
+    
+    comp_number = random.randrange(0,5)
+    
+    #Convert computer's choice to name
+    
+    comp_name = number_to_name(comp_number)
+    
+    #Print computer's choice
+    
+    print "Computer chooses",comp_name 
+    
+    subtract = player_number - comp_number
+    
+    rem = subtract % 5
+    
+    if (rem == 1 or rem == 2):
+        print "Player wins!\n"
+    elif (rem == 3 or rem == 4):
+        print "Computer wins!\n"
+    else:
+        print "Player and computer tie!\n"
+    
+    return 0
+
+
 rpsls("rock")
 rpsls("Spock")
 rpsls("paper")
